80095c04971e6a3e534914a6b43cce462cbffe05
SimoneSperrer <s.sperrer@students.htl-leonding.ac.at>
2026-01-08 14:59:05 +0100

    init swift

---
+++ mobileapp/mobileapp/mobileappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileappApp.swift
+//  mobileapp
+//
+//  Created by Sperrer Simone on 08.01.26.
+//
+
+import SwiftUI
+
+@main
+struct mobileappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobileapp/mobileappTests/mobileappTests.swift
@@ -0,0 +1,17 @@
+//
+//  mobileappTests.swift
+//  mobileappTests
+//
+//  Created by Sperrer Simone on 08.01.26.
+//
+
+import Testing
+@testable import mobileapp
+
+struct mobileappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
